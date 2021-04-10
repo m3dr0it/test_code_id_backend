82acf1bd65ccb6bb680e9a136f78855f615523a6
root <amdtkj@gmail.com>
2021-04-10 10:38:21 +0700

    delete some code

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(cors())
-app.use((req,res,next) => {
-  
-})
+
 app.use((req,res,next) => {
     req.context = {db_conn}
     next()  
